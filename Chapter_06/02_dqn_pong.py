2ca33d7a5a3d4cbf2f651c542c454676d54020d5
Sann-Htet <sannhtet899@gmail.com>
2025-12-23 16:39:01 +0630

    append an experience in a experience buffer

--- Chapter_06/02_dqn_pong.py
+++ Chapter_06/02_dqn_pong.py
@@ -31,3 +31,78 @@ EPSILON_DECAY_LAST_FRAME = 150000
 EPSILON_START = 1.0
 EPSILON_FINAL = 0.01
 
+State = np.ndarray
+Action = int
+BatchTensors = tt.Tuple[
+    torch.ByteTensor,        # current state
+    torch.LongTensor,        # actions
+    torch.Tensor,            # rewards
+    torch.BoolTensor,        # done || trunc
+    torch.ByteTensor         # next state
+]
+
+@dataclass
+class Experience:
+    state: State
+    action: Action
+    reward: float
+    done_trunc: bool
+    next_state: State
+
+
+class ExperienceBuffer:
+    def __init__(self, capacity: int):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+    
+    def append(self, experience: Experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size: int) -> tt.List[Experience]:
+        indices = np.random.choice(len(self), batch_size, replace=False)
+        return [self.buffer[idx] for idx in indices]
+    
+
+class Agent:
+    def __init__(self, env: gym.Env, exp_buffer: ExperienceBuffer):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self.state: tt.Optional[np.ndarray] = None
+        self._reset()
+
+    def _reset(self):
+        self.state, _ = self.env.reset()
+        self.total_reward = 0.0
+
+    @torch.no_grad()
+    def play_step(self, net: dqn_model.DQN, device: torch.device,
+                  epsilon: float = 0.0) -> tt.Optional[float]:
+        done_reward = None
+
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            state_v = torch.as_tensor(self.state).to(device)
+            state_v.unsqueeze_(0)
+            q_vals_v = net(state_v)
+            _, act_v = torch.max(q_vals_v, dim=1)
+            action = int(act_v.item())
+        
+        # do step in the environment
+        new_state, reward, is_done, is_tr, _ = self.env.step(action)
+        self.total_reward += reward
+
+        exp = Experience(
+            state=self.state, action=action, reward=float(reward),
+            done_trunc=is_done or is_tr, new_state=new_state
+        )
+        self.exp_buffer.append(exp)
+        self.state = new_state
+        if is_done or is_tr:
+            done_reward = self.total_reward
+            self.reset()
+        return done_reward
+    
+
